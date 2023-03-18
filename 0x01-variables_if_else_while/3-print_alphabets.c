8aa084ef936dd02014fa671302761f75243f7fef
Usman-9895 <mbackeousmane31@gmail.com>
2023-03-18 02:37:57 +0000

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -7,17 +8,22 @@
 int main(void)
 {
 char c = 'a';
+
 while (c <= 'z')
 {
 putchar(c);
 c++;
 }
+
 c = 'A';
+
 while (c <= 'Z')
 {
 putchar(c);
 c++;
 }
+
 putchar('\n');
+
 return (0);
-}		
+}
