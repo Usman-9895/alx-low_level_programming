c1092ea08ace568f8601b065f93651affa52dad2
Usman-9895 <mbackeousmane31@gmail.com>
2023-03-21 23:50:31 +0000

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
+
 /**
 * _islower 
 *  La fonction vérifie les caractaire minuscule
 * Return: 1 si la chaine de caractaire est miniscule, sinon 0.
 */
+
 
 int _islower(int c)
 {
