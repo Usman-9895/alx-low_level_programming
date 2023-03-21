b406642a3203aded591de974aebf299e69fc6350
Usman-9895 <mbackeousmane31@gmail.com>
2023-03-21 18:20:24 +0000

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
 
+/**
+ * main - this is the main function
+ *
+ * Return: 0 when successfull
+ */
 int main(void)
 {
-const char* str = "_putchar";
-for (int i = 0; str[i] != '\0'; i++)
+char str[] = "_putchar";
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
 {
 putchar(str[i]);
 }
+
 putchar('\n');
 return (0);
 }
