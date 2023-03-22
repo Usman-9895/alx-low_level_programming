fef177f5dab5af31a7e7affd04e82c869bbfef1e
Usman-9895 <mbackeousmane31@gmail.com>
2023-03-22 01:04:02 +0000

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * print_sign - imprime le signe d'un nombre
- * @n : l'entier à vérifier
- * Renvoie : 1 et imprime + si n est supérieur à zéro
- * 0 et imprime 0 si n vaut zéro
- * -1 et imprime - si n est inférieur à zéro
-*/
-
+ * print_sign - prints the sign of a number
+ * @n: the int to check
+ * Return: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
+ */
 int print_sign(int n)
 {
 if (n > 0)
