ac397f94a2f21dd120a6e154cce97b44f1da8902
Usman-9895 <mbackeousmane31@gmail.com>
2023-04-09 23:22:22 +0000

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
 #include "main.h"
 
-/**		
- * _atoi - converts a string to an integer		
+/**
+ * _atoi - converts a string to an integer
  * @s: string to be converted
  *
- * Return: the int converted from the string		
+ * Return: the int converted from the string
  */
-		
+
 int _atoi(char *s)
-		
+
 {
 	int i, d, n, len, f, digit;
-		
+
 	i = 0;
-	d = 0;	
-	n = 0;	
-	len = 0;	
+	d = 0;
+	n = 0;
+	len = 0;
 	f = 0;
-	digit = 0;	
-		
+	digit = 0;
+
 	while (s[len] != '\0')
 		len++;
 	while (i < len && f == 0)
 	{
-		if (s[i] == '-')	
+		if (s[i] == '-')
 			++d;
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			digit = s[i] - '0';		
+			digit = s[i] - '0';
 			if (d % 2)
-				digit = -digit;		
+				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
@@ -38,8 +38,8 @@ int _atoi(char *s)
 			f = 0;
 		}
 		i++;
-	}	
-	if (f == 0)	
+	}
+	if (f == 0)
 		return (0);
 	return (n);
 }
@@ -61,7 +61,7 @@ int nb1, nb2, res;
 	{
 	printf("Error\n");
 	return (1);
-	}	
+	}
 	nb1 = _atoi(argv[1]);
 	nb2 = _atoi(argv[2]);
 	res = nb1 * nb2;
