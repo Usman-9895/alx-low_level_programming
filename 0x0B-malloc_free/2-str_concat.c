91f4c711d50dd515ec9e39c4781d233125563545
Usman-9895 <mbackeousmane31@gmail.com>
2023-04-13 00:25:20 +0000

    2. He who is not courageous enough to take risks will accomplis

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,34 +1,44 @@
 #include "main.h"
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * str_concat - Concatenates two strings.
- * @s1: First string to concatenate.
- * @s2: Second string to concatenate.
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
  *
- * Return: Pointer to newly allocated space in memory which contains the
- *         contents of s1, followed by the contents of s2, and null terminated.
- *         NULL on failure.
+ * Return: pointer to newly allocated space in memory with concatenated string,
+ *         or NULL on failure
  */
+
 char *str_concat(char *s1, char *s2)
 {
+	unsigned int len1 = 0, len2 = 0;
+	char *concat_str;
+	unsigned int i;
+
 	if (s1 == NULL)
-	s1 = "";
+		s1 = "";
 	if (s2 == NULL)
-	s2 = "";
+		s2 = "";
 
-	size_t s1_len = strlen(s1);
-	size_t s2_len = strlen(s2);
+	while (s1[len1] != '\0')
+		len1++;
 
-	char *result = malloc(s1_len + s2_len + 1);
+	while (s2[len2] != '\0')
+		len2++;
 
-	if (result == NULL)
-	return NULL;
+	concat_str = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (concat_str == NULL)
+		return (NULL);
 
-	memcpy(result, s1, s1_len);
-	memcpy(result + s1_len, s2, s2_len + 1);
+	for (i = 0; i < len1; i++)
+		concat_str[i] = s1[i];
 
-	return result;
+	for (i = 0; i < len2; i++)
+		concat_str[len1 + i] = s2[i];
+
+	concat_str[len1 + len2] = '\0';
+
+	return (concat_str);
 }
