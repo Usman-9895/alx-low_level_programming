b24cf3907f43c62217197b2ea29c9d627b2b535d
Usman-9895 <mbackeousmane31@gmail.com>
2023-04-17 18:21:19 +0000

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef MA_STRUCTURE_H
+#define MA_STRUCTURE_H
+
+/**
+ * struct dog - Display name age and owner
+ * @name: Name dog
+ * @age: age dog
+ * @owner: property dog
+ *
+ * Description: Display dog
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
