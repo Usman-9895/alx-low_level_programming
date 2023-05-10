7606d6161622a586887040f9d1a650826671a36b
Usman-9895 <mbackeousmane31@gmail.com>
2023-05-10 10:46:14 +0000

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,26 +1,33 @@
 #include "main.h"
 
 /**
- * print_binary - prints the binary equivalent of a decimal number
- * @n: number to print in binary
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
-void print_binary(unsigned long int n)
+int create_file(const char *filename, char *text_content)
 {
-	int i, cnt = 0;
-	unsigned long int cur;
+	int fd, w, len = 0;
 
-	for (i = 63; i >= 0; i--)
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
 	{
-		cur = n >> i;
-
-		if (cur & 1)
-		{
-			_putchar('1');
-			cnt++;
-		}
-		else if (cnt)
-			_putchar('0');
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	if (!cnt)
-		_putchar('0');
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
