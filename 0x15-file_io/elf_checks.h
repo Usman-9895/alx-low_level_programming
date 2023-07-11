08cb0dc4afb7854ee2cbfe01b9279740cd3ef1ff
Usman-9895 <mbackeousmane31@gmail.com>
2023-07-11 00:07:28 +0000

    Delete02

--- 0x15-file_io/elf_checks.h
+++
@@ -1,8 +0,0 @@
-#ifndef ELF_CHECKS_H
-#define ELF_CHECKS_H
-
-#include <elf.h>
-
-void check_elf(unsigned char *e_ident);
-
-#endif /* ELF_CHECKS_H */
--- 0x15-file_io/elf_utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef ELF_UTILS_H
-#define ELF_UTILS_H
-
-#include <elf.h>
-
-int open_elf_file(char *filename);
-void close_elf_file(int fd);
-
-#endif /* ELF_UTILS_H */
