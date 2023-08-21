7b8bfc21936e9f5484b11ff842bc4fc275672e50
Aya-oba <mbackeousmane31@gmail.com>
2023-08-21 17:56:40 +0000

    3. Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-cp /path/to/your/winning/numbers/file . && echo "Please make me win!" > gm
+cp /path/to/your/winning/numbers/file .
+echo "Please make me win!" > gm
+chmod +x gm
+sleep 98
+rm 101-make_me_win.sh
